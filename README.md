0b7dc2aecd32b7bbc0c0b5a7204900d7e6c1c8c0
Rodrigo Linfati <rodrigo@linfati.cl>
2023-10-13 09:11:17 -0300

    lilygo_t_display_s3

---
+++ README.md
@@ -0,0 +1,4 @@
+# ESP32-S3 Arduino Examples
+
+Welcome to the ESP32-S3 Arduino Examples repository! Here, you'll find a collection of example projects specifically tailored for the ESP32-S3 microcontroller using the Arduino development environment. The ESP32-S3 is a versatile microcontroller that offers Wi-Fi and Bluetooth connectivity, making it an ideal choice for a wide range of IoT and embedded systems applications.
+
---
+++ lilygo_t_display_s3/src/main.cpp
@@ -0,0 +1,123 @@
+#include <esp_wifi.h>
+#include <WiFiProv.h>
+#include <HTTPClient.h>
+#include "TFT_eSPI.h"
+#include "libpax_api.h"
+#include "OneButton.h"
+
+TFT_eSPI tft = TFT_eSPI();
+struct count_payload_t count_from_libpax;
+unsigned long blecount = 0;
+unsigned long wificount = 0;
+OneButton button(BUTTON_RST);
+unsigned long start = 0;
+unsigned long timeoutms = 60 * 1e3;
+String ippub = "0.0.0.0";
+
+void process_count(void) {
+  blecount = count_from_libpax.ble_count;
+  wificount = count_from_libpax.wifi_count;
+  return;
+}
+
+void setup() {
+  tft.init();
+  tft.setTextSize(2);
+  tft.setRotation(3);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_LIGHTGREY);
+
+  button.attachClick([]() {
+    tft.fillScreen(TFT_GREEN);
+    esp_restart();
+  });
+
+  button.attachDoubleClick([]() {
+    tft.fillScreen(TFT_RED);
+    wifi_config_t conf;
+    conf.sta.ssid[0] = 0;
+    esp_wifi_set_config((wifi_interface_t)ESP_IF_WIFI_STA, &conf);
+    esp_restart();
+  });
+
+  WiFiProv.beginProvision();
+
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(0, 0);
+  start = millis();
+  tft.printf("WiFi");
+  while ( millis()-start < timeoutms) {
+    tft.printf(".");
+    delay(1000);
+    if ( WiFi.isConnected() ) break;
+  }
+
+  WiFi.enableIpV6();
+  configTzTime("<-04>4<-03>,M9.1.6/24,M4.1.6/24", "time.cloudflare.com", "ntp.ubiobio.cl");
+
+  tft.fillScreen(TFT_DARKGREY);
+  tft.setCursor(0, 0);
+  start = millis();
+  tft.printf("SNTP");
+  while ( millis()-start < timeoutms) {
+    tft.printf(".");
+    struct tm timeinfo;
+    if ( getLocalTime(&timeinfo) ) break;
+  }
+
+  if ( !WiFi.isConnected() ) {
+    tft.fillScreen(TFT_RED);
+    tft.setCursor(0, 0);
+    tft.printf("Erasing Provision...");
+    wifi_config_t conf;
+    conf.sta.ssid[0] = 0;
+    esp_wifi_set_config((wifi_interface_t)ESP_IF_WIFI_STA, &conf);
+    delay(60 * 1e3);
+    esp_restart();
+  }
+
+  HTTPClient http;
+  http.begin("http://ifconfig.me/ip");
+  if ( http.GET() == HTTP_CODE_OK ) ippub = http.getString();
+  
+  struct libpax_config_t configpax;
+  libpax_default_config(&configpax);
+  configpax.wifi_channel_map = WIFI_CHANNEL_ALL;
+  configpax.wificounter = 0;
+  configpax.blecounter = 1;
+  libpax_update_config(&configpax);
+  libpax_counter_init(process_count, &count_from_libpax, 60, 0); 
+  libpax_counter_start();
+
+  return;
+}
+
+void loop() {
+  button.tick();
+
+  if ( millis()-start < 1e3 ) {
+    return;
+  }
+  start = millis();
+
+  struct tm timeinfo;
+  getLocalTime(&timeinfo);
+    
+  tft.fillScreen(TFT_DARKGREY);
+  tft.setCursor(0, 0);
+  tft.printf("\n");
+  tft.printf(" %04i-%02i-%02i  ",
+    1900+timeinfo.tm_year, 1+timeinfo.tm_mon, timeinfo.tm_mday);
+  tft.printf(" %02i:%02i:%02i\n", 
+    timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+  tft.printf("\n");
+  tft.printf("  SSID: %s\n", WiFi.SSID().c_str());
+  tft.printf(" BSSID: %s\n", WiFi.BSSIDstr().c_str());
+  tft.printf("\n");
+  tft.printf(" IPprv: %s\n", WiFi.localIP().toString().c_str());
+  tft.printf(" IPpub: %s\n", ippub.c_str());
+  tft.printf("\n");
+  tft.printf(" # BLE: %i\n", blecount);
+  
+  return;
+}
